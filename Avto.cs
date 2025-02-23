dbb1614106197e975ea18befaac149e2f6697eef
Limon <Limon@DAVID>
2025-02-23 17:33:55 +0200

    Добавьте файлы проекта.

---
+++ Avto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_24
+{
+    internal class Avto
+    {
+        string brand;
+        string color;
+        double skor;
+
+        public double Skor
+        {
+            get { return skor; }
+            set
+            {
+
+                if (value < 20)
+                {
+                    skor = 20;
+                }
+                else if (value > 120)
+                {
+                    skor = 120;
+                }
+                else
+                {
+                    skor = value;
+                }
+            }
+        }
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+
+        public Avto() //Без параметров
+        {
+            brand = "BMW";
+            color = "red";
+            skor = 90;
+        }
+        public Avto(string brand, string color, double skor) // C параметрами
+        {
+            this.Brand = brand;
+            this.Color = color;
+            this.Skor = skor;
+        }
+
+        public string Color //Свойство для цвета автомобиля
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public override string ToString()
+        {
+            return $"Марка: {brand}, Цвет: {color}, Скорость: {skor} км/ч";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto = new Avto();
+            avto.Brand = "BMW";
+            avto.Color = "Red";
+            avto.Skor = 90;
+            Console.WriteLine(avto.ToString());
+            Console.WriteLine();
+
+            Avto avto1 = new Avto("Bmw", "Red", 80);
+            Console.WriteLine(avto1);
+            Console.WriteLine();
+
+            Console.WriteLine("Введите марку машины: ");
+            string brend = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите цвет: ");
+            string color = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Введите скорость: ");
+            double skor = Convert.ToDouble(Console.ReadLine());
+
+            Avto avto2 = new Avto(brend, color, skor);
+            Console.WriteLine(avto2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
